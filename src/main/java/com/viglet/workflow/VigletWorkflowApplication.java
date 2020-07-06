6381025baead667448e096a279a16186cee3d90d
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-07-06 12:43:53 -0300

    - Initial Commit

---
+++ src/main/java/com/viglet/workflow/VigletWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.viglet.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigletWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigletWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viglet/workflow/VigletWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viglet.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigletWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
